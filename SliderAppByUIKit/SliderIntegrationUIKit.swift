9e2ce6bd199d3e2e1be102b6061b4cd97945f5ce
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-10-30 23:42:05 +0300

    first_commit

---
+++ SliderAppByUIKit/SliderIntegrationUIKit.swift
@@ -0,0 +1,20 @@
+//
+//  SliderIntegrationUIKit.swift
+//  SliderAppByUIKit
+//
+//  Created by Pavel Krigin on 30.10.22..
+//
+
+import SwiftUI
+
+struct SliderIntegrationUIKit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderIntegrationUIKit_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderIntegrationUIKit()
+    }
+}
