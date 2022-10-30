02c1634d4ade95d10a750cd3b20c4ceab58f9d1e
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-10-30 23:32:04 +0300

    Initial Commit

---
+++ SliderAppByUIKit/SliderAppByUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppByUIKitApp.swift
+//  SliderAppByUIKit
+//
+//  Created by Pavel Krigin on 30.10.22..
+//
+
+import SwiftUI
+
+@main
+struct SliderAppByUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
